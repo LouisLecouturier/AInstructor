aad0228d95099a7576410df9b2fd406b286258d6
Mael Monteil <monteil.m.t@gmail.com>
2023-05-31 10:42:33 +0200

    ModelBDD_completed_+_AdminpageCRUD

--- server/AInstructor/app/admin.py
+++ server/AInstructor/app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import * 
+from django.apps import apps
 
-# Register your models here.
+
+models = apps.get_app_config('app').get_models()
+
+
+class AdminFields(admin.ModelAdmin):
+    def get_list_display(self, request):
+        model = self.model
+        fields = [field.name for field in model._meta.fields]
+        print(model)
+        print(fields)
+        return fields
+
+
+for model in models:
+    try:
+        admin.site.register(model, AdminFields)
+    except Exception:
+        print("Erreur pas de rendu pour le model model:", model)
+        print(Exception)
+        pass
+
