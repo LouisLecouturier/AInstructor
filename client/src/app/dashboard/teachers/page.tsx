904b13304088cfd258bd227be777d15a220c94a3
Nicolo41 <72193849+Nicolo41@users.noreply.github.com>
2023-06-05 18:11:30 +0200

    QuestionEditor

--- client/src/app/dashboard/teachers/page.tsx
+++ client/src/app/dashboard/teachers/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/Interactions/Button";
 import QuestionEdit from "@/components/dashboard/Questions/QuestionEditor";
 
 const questions = [
@@ -13,18 +14,6 @@ const questions = [
   },
   {
     id: 3,
-    question: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  },
-  {
-    id: 4,
-    question: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  },
-  {
-    id: 5,
-    question: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-  },
-  {
-    id: 6,
     question: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
   },
 ];
@@ -44,23 +33,28 @@ const onEdit = (questionNumber: number) => {
 export default function Dashboard({}) {
   return (
     <>
-      <header>
-        <h1 className={"flex items-center h-16 text-5xl font-black"}>
-          Dashboard
-        </h1>
-        <div className="flex flex-col gap-4 my-20px">
-          {questions.map((question, index) => (
-            <QuestionEdit
-              key={question.id}
-              index={index}
-              question={question.question}
-              onAccept={() => onAccept(index)}
-              onDelete={() => onDelete(index)}
-              onEdit={() => onEdit(index)}
-            />
-          ))}
-        </div>
-      </header>
+      <div className="flex flex-col gap-10">
+        <header>
+          <h1 className={"flex items-center h-16 text-5xl font-black"}>
+            Dashboard
+          </h1>
+          <div className="flex flex-col gap-4 my-20px">
+            {questions.map((question, index) => (
+              <QuestionEdit
+                key={question.id}
+                index={index}
+                question={question.question}
+                onAccept={() => onAccept(index)}
+                onDelete={() => onDelete(index)}
+                onEdit={() => onEdit(index)}
+              />
+            ))}
+          </div>
+        </header>
+        <Button className="w-96 bg-green-700" size="md" rounded="sm">
+          Confirm
+        </Button>
+      </div>
     </>
   );
 }
