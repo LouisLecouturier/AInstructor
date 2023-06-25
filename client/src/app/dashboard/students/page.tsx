0a3d71db4b620a7ab7fb39c4f4311f091bb62159
Louis Lecouturier <71454059+LouisLecouturier@users.noreply.github.com>
2023-06-25 17:04:46 +0200

    quick trucs avant queje mette sur ma branche

--- client/src/app/dashboard/students/page.tsx
+++ client/src/app/dashboard/students/page.tsx
@@ -9,6 +9,7 @@ import Container from "@components/Layout/Container";
 import { nanoid } from "nanoid";
 import { useQuery } from "@tanstack/react-query";
 import { getUserCoursesStats } from "@/requests/stats";
+import ContainerMessage from "@components/Layout/Container/ContainerMessage";
 
 const Dashboard = () => {
   const { data: session } = useSession();
@@ -30,7 +31,7 @@ const Dashboard = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(coursesStats);
+  const finishedCourses = coursesStats.filter((c) => c.progress === 100);
 
   return (
     <div className="flex flex-1 w-full flex-col">
@@ -46,26 +47,22 @@ const Dashboard = () => {
         >
           <div className="flex flex-col gap-2">
             <div className="flex gap-8 ">
-              {coursesStats.map((course, index) => {
-                if (index < 3 && course.progress < 100) {
-                  console.log(course);
-                  return (
-                    <>
-                      <QuestionCube
-                        key={nanoid()}
-                        index={index}
-                        course={course.course.name}
-                        deliveryDate={course.course.deliveryDate}
-                        creationDate={course.course.creationDate}
-                        progress={course.progress}
-                        href={`/dashboard/students/courses/${course.course.uuid}`}
-                        status={course.course.status}
-                        // image={course.image}
-                      />
-                    </>
-                  );
-                }
-              })}
+              {coursesStats
+                .filter((c) => c.progress < 100)
+                .slice(0, 2)
+                .map((course, index) => (
+                  <QuestionCube
+                    key={nanoid()}
+                    index={index}
+                    course={course.course.name}
+                    deliveryDate={course.course.deliveryDate}
+                    creationDate={course.course.creationDate}
+                    progress={course.progress}
+                    href={`/dashboard/students/courses/${course.course.uuid}`}
+                    status={course.course.status}
+                    // image={course.image}
+                  />
+                ))}
               {coursesStats.length > 3 && <QuestionCube isSeeAll />}
               {(coursesStats.length === 0 ||
                 coursesStats.every((item) => item.progress >= 100)) && (
@@ -78,41 +75,43 @@ const Dashboard = () => {
           </div>
         </Container>
 
-        <div className="flex flex-col gap-2">
-          <Container
-            title="Finished courses"
-            description={"Access to your old courses"}
-          >
-            <div className="flex flex-col gap-2 w-full">
-              {coursesStats.map((course: any) => {
-                if (course.progress >= 100) {
-                  const properties = [
-                    {
-                      label: "Creation date",
-                      value: course.course.creationDate,
-                    },
-                    {
-                      label: "Delivery date",
-                      value: course.course.deliveryDate,
-                    },
-                    { label: "Progress", value: course.progress + "%" },
-                  ];
+        <Container
+          title="Finished courses"
+          description={"Access to your old courses"}
+        >
+          <div className="flex flex-col gap-2 w-full">
+            {finishedCourses.length > 0 ? (
+              finishedCourses.map((course: any) => {
+                const properties = [
+                  {
+                    label: "Creation date",
+                    value: course.course.creationDate,
+                  },
+                  {
+                    label: "Delivery date",
+                    value: course.course.deliveryDate,
+                  },
+                  { label: "Progress", value: course.progress + "%" },
+                ];
 
-                  return (
-                    <ListItem
-                      href={"/dashboard/students/courses/1"}
-                      key={nanoid()}
-                      properties={properties}
-                      status={course.course.status}
-                    >
-                      {course.course.name}
-                    </ListItem>
-                  );
-                }
-              })}
-            </div>
-          </Container>
-        </div>
+                return (
+                  <ListItem
+                    href={"/dashboard/students/courses/1"}
+                    key={nanoid()}
+                    properties={properties}
+                    status={course.course.status}
+                  >
+                    {course.course.name}
+                  </ListItem>
+                );
+              })
+            ) : (
+              <ContainerMessage>
+                You haven&apos;t finished any course yet
+              </ContainerMessage>
+            )}
+          </div>
+        </Container>
       </div>
     </div>
   );
---
+++ client/src/components/Dashboard/Students/Courses/CoursePresentation/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CoursePresentation = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default CoursePresentation;
